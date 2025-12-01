b3b4a27b499323e473047ee01afe03cf11e2123e
doronKudra <doron0505@gmail.com>
2025-12-01 15:23:59 +0200

    Bug Service And API CRUDL

--- server.js
+++ server.js
@@ -1,4 +1,61 @@
 import express from 'express'
+import { bugService } from './services/bug.service.js'
+
 const app = express()
-app.get('/', (req, res) => res.send('Hello there'))
+app.use(express.static('public'))
+
 app.listen(3030, () => console.log('Server ready at port 3030'))
+
+app.get('/api/bug', (req, res) => {
+    bugService.query()
+        .then(bugs => {
+            res.json(bugs)
+        })
+        .catch(err => {
+            res.status(400).send(err, 'Cannot get bugs :(')
+        })
+})
+
+
+app.get('/api/bug/save', (req, res) => {
+    const bug = {
+        _id: req.query._id,
+        title: req.query.title,
+        description: req.query.desc,
+        severity: +req.query.sev,
+    }
+
+    const func = (bug._id) ? 'update' : 'add'
+    bugService[func](bug)
+        .then((savedBug) => {
+            res.json(savedBug)
+        })
+        .catch(err => {
+            res.status(400).send(err,'Cannot save bug')
+        })
+})
+
+
+app.get('/api/bug/:bugId', (req, res) => { 
+    const { bugId } = req.params
+    bugService.getById(bugId)
+        .then(bug =>{
+            res.json(bug)
+        })
+        .catch(err => {
+            res.status(400).send(err,'Cannot find bug')
+        })
+})
+
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const { bugId } = req.params
+    bugService.remove(bugId)
+        .then(() => {
+            res.send('Bug removed')
+        })
+        .catch(err => {
+            res.status(400).send(err,'Cannot remove bug')
+        })
+})
+
---
+++ services/bug.service.js
@@ -0,0 +1,64 @@
+import fs from 'fs'
+import {utilService} from './util.service.js'
+
+export const bugService = {
+    query,
+    getById,
+    remove,
+    add,
+    update
+}
+
+const bugsFile = 'data/bug.json'
+const bugs = utilService.readJsonFile(bugsFile)
+
+function query() {
+    if(!bugs) return Promise.reject('Error retrieving bugs \n try again later...')
+    return Promise.resolve(bugs)
+}
+
+function getById(bugId) {
+    const bug = bugs.find(bug => bug._id === bugId)
+    if (!bug) return Promise.reject(`Bug not found: (${bugId})`)
+    return Promise.resolve(bug)
+}
+
+function remove(bugId) {
+    const idx = bugs.findIndex(bug => bug._id === bugId)
+    if (idx === -1) return Promise.reject(`Bug not found (${bugId})`)
+    bugs.splice(idx, 1)
+    return _saveBugs()
+    
+}
+
+function add({title,description,severity}) {
+    const bugToSave = {
+        _id: utilService.makeId(),
+        title,
+        description,
+        severity,
+        createdAt: Date.now()
+    }
+    bugs.push(bugToSave)
+    return _saveBugs().then(()=>bugToSave)
+}
+
+function update(bug) {
+    const bugToUpdate = bugs.find(currBug => currBug._id === bug._id )
+    if (!bugToUpdate) return Promise.reject(`Bug not found (${bug._id})`)
+    bugToUpdate.title = bug.title
+    bugToUpdate.description = bug.description
+    bugToUpdate.severity = bug.severity
+    return _saveBugs().then(()=>bugToUpdate)
+}
+
+function _saveBugs() {
+    return new Promise((resolve, reject)=>{
+        const strBugs = JSON.stringify(bugs, null, 2)
+        fs.writeFile(bugsFile, strBugs, (err)=>{
+            if (err) return reject(err,' bug encountered while trying to update bug file')
+            resolve()    
+        })
+    })
+}
+
